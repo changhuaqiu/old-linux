79f19cf5334a4de9929ea390fabc9f0f4c6af58a
clclcl888 <qiufanhua4960788@qq.com>
2021-01-24 17:17:46 +0800

    Initial Commit

---
+++ server-linux/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  server-linux
+//
+//  Created by 邱昌华 on 2021/1/24.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
