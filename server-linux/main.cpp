2da5efdd094033ea2a7c1eed4440486d8780b84d
clclcl888 <qiufanhua4960788@qq.com>
2021-02-01 22:21:02 +0800

    first change

--- server-linux/main.cpp
+++ server-linux/main.cpp
@@ -6,9 +6,31 @@
 //
 
 #include <iostream>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <assert.h>
+#include <stdio.h>
+#include <unistd.h>
+#include <errno.h>
+#include <string.h>
+#include <fcntl.h>
+#include <stdlib.h>
+#include <signal.h>
+#include <time.h>
+
+static bool stop = false;
+
+
 
 int main(int argc, const char * argv[]) {
     // insert code here...
-    std::cout << "Hello, World!\n";
+   
+    std::cout<<"hello world"<<std::endl;
+    std::cout<<"a new state"<<std::endl;
     return 0;
+    
+    
+    
 }
