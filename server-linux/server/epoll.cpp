1660d5f6ef94e944051cf2a27f404d7f74a4c7fd
clclcl888 <qiufanhua4960788@qq.com>
2021-02-02 00:37:19 +0800

    adde  epoll file to server

---
+++ server-linux/server/epoll.cpp
@@ -0,0 +1,8 @@
+//
+//  epoll.cpp
+//  server-linux
+//
+//  Created by 邱昌华 on 2021/2/2.
+//
+
+#include "epoll.hpp"
---
+++ server-linux/server/epoll.hpp
@@ -0,0 +1,13 @@
+//
+//  epoll.hpp
+//  server-linux
+//
+//  Created by 邱昌华 on 2021/2/2.
+//
+
+#ifndef epoll_hpp
+#define epoll_hpp
+
+#include <stdio.h>
+
+#endif /* epoll_hpp */
